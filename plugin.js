d9346dbb14772ec0d6e792be886985bb6f6dd1da
viktor-punko <punko.viktor@yandex.ru>
2014-07-24 21:27:15 +0300

    css-file instance of js set css

--- plugin.js
+++ plugin.js
@@ -5,36 +5,57 @@ id_modal_div - div with tab content.
 */
 (function( $ ){
 
-  $.fn.tabcontrol = function( ul_selector ) {  
+	var methods = {
+		init: function() {
+			$("head").append(
+				"<link href='styles_plugin_tab_control.css' type='text/css' rel='stylesheet' />");
+		},
 
-  	// использование Math.round() даст неравномерное распределение!
-	// function getRandomInt(min, max){
-	//   return Math.floor(Math.random() * (max - min + 1)) + min;
-	// }
+		show: function( ul_selector ) {  
 
-	// var randomTabContentClassName = "tab-content" + getRandomInt(1, 1000);
+		  	this.after('<div id="tab_content_tabcontrol_plugin"></div>');
 
-  	this.after('<div id="tab_content"></div>');
+			this.attr('id', 'ul_nav_tabcontrol_plugin');
 
-	var listItems = this.children();
-	var aTags = listItems.map(function (i, item, arr) {
-		return $(item).children()[0];
-	});
+			var widthDivTabContent = this.width();
+			$('#tab_content_tabcontrol_plugin').css('width', 
+				widthDivTabContent + 'px');
 
-	aTags.each(function () {
-		$(this).css('text-decoration', 'none');
+			var listItems = this.children();
+			var aTags = listItems.map(function (i, item, arr) {
+				return $(item).children()[0];
+			});
 
-		$(this).on('click', function () {
-			//a.stopPropagation();
-			var idDiv = $(this).attr('href');
-			console.log(idDiv);
-			var htmlCurrentDiv = $('#' + idDiv).html();
-			$('#tab_content').html(htmlCurrentDiv);
-			return false;
-		});
-	});
+			aTags.each(function () {
+				$(this).css('text-decoration', 'none');
+				var idDiv = $(this).attr('href');
 
-	return listItems;
+				$('#' + idDiv).css('display', 'none');
 
-  };
+				$(this).on('click', function () {
+					console.log(idDiv);
+					var htmlCurrentDiv = $('#' + idDiv).html();
+					$('#tab_content_tabcontrol_plugin').html(htmlCurrentDiv);
+					return false;
+				});
+			});
+
+			return listItems;
+
+		}
+
+	};
+
+	$.fn.tabcontrol = function( method ) {
+
+		// логика вызова метода
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Метод с именем ' +  method + ' не существует для jQuery.tabcontrol' );
+		} 
+	};
+
 })( jQuery );
